5810090fe171ff3fa6f1da018b60550649f2c572
tobiasbianchi <tobias.bianchi93@gmail.com>
2018-09-02 13:33:54 -0300

    working on day/night/no_connectivity_theme

--- app/src/main/java/com/tdp2/tp0/weweather/activities/FiveDayTemperatureActivity.java
+++ app/src/main/java/com/tdp2/tp0/weweather/activities/FiveDayTemperatureActivity.java
@@ -1,8 +1,11 @@
 package com.tdp2.tp0.weweather.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,21 +24,39 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class FiveDayTemperatureActivity extends AppCompatActivity {
+public class FiveDayTemperatureActivity extends AppCompatActivity
+{
+    enum DisplayState
+    {
+        NO_CONNECTIVITY(R.style.AppTheme_NoConnectivity),
+        NIGHT_MODE(R.style.AppTheme_NightMode),
+        DAY_MODE(R.style.AppTheme_DayMode);
+        int theme;
+        DisplayState(int theme)
+        {
+            this.theme = theme;
+        }
+    }
 
+    private FloatingActionButton actionButton;
+    private DisplayState displayState = DisplayState.NO_CONNECTIVITY;
     private DayListAdapter adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        //setTheme(displayState.theme);
         setContentView(R.layout.activity_pantalla_de_inicio);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        actionButton= findViewById(R.id.fab);
+        actionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //TODO make actualization of temperatures
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -59,8 +80,43 @@ public class FiveDayTemperatureActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        switch(id)
+        {
+            case R.id.action_search:
+                Intent intent = new Intent(this, SearchCitiyActivity.class);
+                startActivity(intent);
+                return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setNightMode()
+    {
+        changeDisplayMode(DisplayState.NIGHT_MODE, true);
+    }
+
+    public void setDayMode()
+    {
+        changeDisplayMode(DisplayState.DAY_MODE, true);
+    }
+
+    public void setNoConnectivityMode()
+    {
+       changeDisplayMode(DisplayState.NO_CONNECTIVITY, false);
+    }
+
+    private void changeDisplayMode(DisplayState state, boolean displayTemperatures)
+    {
+        displayState = state;
+        displayRefresh(displayTemperatures);
+        adapter.changeDisplayTemperature(displayTemperatures);
+        recreate();
+    }
+
+    private void displayRefresh(boolean display)
+    {
+        int visibility = display ? View.VISIBLE : View.GONE;
+        actionButton.setVisibility(visibility);
     }
 
     private List<DayTemperatureViewModel> getDateList()
--- app/src/main/java/com/tdp2/tp0/weweather/activities/SearchCitiyActivity.java
+++ app/src/main/java/com/tdp2/tp0/weweather/activities/SearchCitiyActivity.java
@@ -2,14 +2,77 @@ package com.tdp2.tp0.weweather.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.tdp2.tp0.weweather.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchCitiyActivity extends AppCompatActivity
 {
+    private SearchView searchView;
+    private ArrayAdapter<String> cityAdapter;
+    private List<String> cityList;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_city);
+
+        searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                if( query.length() >= 3 )
+                {
+                    //TODO make request for cities
+                    return true;
+                }
+                Toast.makeText(SearchCitiyActivity.this,
+                        R.string.not_enough_letters, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                return false;
+            }
+        });
+
+        cityList = new ArrayList<>();
+        cityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1 ,cityList);
+        ListView listView = findViewById(R.id.cities_list);
+        listView.setAdapter(cityAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                if( position <= cityList.size() )
+                {
+                    String selected = cityList.get(position);
+                    //TODO set city selected..
+                }
+            }
+        });
+
+        //onCitiesLoaded();
+    }
+
+
+    public void onCitiesLoaded(List<String> cities)
+    {
+        cityList.clear();
+        cities.addAll(cities);
+        cityAdapter.notifyDataSetChanged();
     }
 }
