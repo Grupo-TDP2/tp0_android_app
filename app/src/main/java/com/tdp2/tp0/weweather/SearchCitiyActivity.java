a557473961be097d2783556720f762bb837335a9
tobiasbianchi <tobias.bianchi93@gmail.com>
2018-09-01 21:24:56 -0300

    Initialized Android project with icons and layouts

---
+++ app/src/main/java/com/tdp2/tp0/weweather/SearchCitiyActivity.java
@@ -0,0 +1,13 @@
+package com.tdp2.tp0.weweather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchCitiyActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_city);
+    }
+}
